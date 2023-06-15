7b89183c8b42bb42ab47a6d6b923a6ef93e8b1d8
faikxsahin <faikxsahin@outlook.com>
2023-06-15 19:41:42 +0300

    Added dropping object with timer

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+
+    // Caching Reference
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+
+    [SerializeField] float timeToWait = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeToWait)
+        {
+            Debug.Log("5 seconds has elapsed");
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
+    }
+
+}
